35bc6e72697580b66071d9305eae130fac153330
victor moye <51638222+VictorMoye@users.noreply.github.com>
2023-06-01 18:23:01 -0300

    inserindo registros relacionados

--- modelagem avançando na primeira forma/criando o banco de cursos.sql
+++ modelagem avançando na primeira forma/criando o banco de cursos.sql
@@ -27,5 +27,19 @@ foreign key (fk_idaluno) references aluno(idaluno);
 
 # criando a outra referencia com a tabela curso 
 alter table aluno_curso add constraint fk_idcurso_alunocurso
-foreign key (fk_idcurso) references curso(idcurso)
+foreign key (fk_idcurso) references curso(idcurso);
 
+# Inserindo registros relacionados 
+select * from aluno;
+select * from curso;
+
+insert into aluno_curso(fk_idaluno, fk_idcurso)values(1, 3);
+insert into aluno_curso(fk_idaluno, fk_idcurso)values(1, 4);
+insert into aluno_curso(fk_idaluno, fk_idcurso)values(2, 2);
+insert into aluno_curso(fk_idaluno, fk_idcurso)values(3, 1);
+insert into aluno_curso(fk_idaluno, fk_idcurso)values(3, 2);
+insert into aluno_curso(fk_idaluno, fk_idcurso)values(3, 3);
+insert into aluno_curso(fk_idaluno, fk_idcurso)values(3, 4);
+insert into aluno_curso(fk_idaluno, fk_idcurso)values(4, 1);
+
+select * from aluno_curso;
