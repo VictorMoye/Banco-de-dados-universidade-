77e0f28a11932100bb0d4ea4883e0c5edb18aa7d
victor moye <51638222+VictorMoye@users.noreply.github.com>
2023-06-01 18:18:08 -0300

    criando a tabela de relacionamento fraco aluno_cursos, criando os seus relacionamentos atraves das chaves estrangeiras para manter a normalidade do banco de dados e mechendo no br modelo para manter o progresso salvo

--- modelagem avançando na primeira forma/criando o banco de cursos.sql
+++ modelagem avançando na primeira forma/criando o banco de cursos.sql
@@ -11,4 +11,21 @@ values('Curso Completo do Desenvolvedor NodeJS e MongoDB'),
 ('Desenvolvimento Web Completo 2023');
 
 select *
-from curso
+from curso;
+
+# criando a tabela de relacionamento fraco aluno cursos 
+create table aluno_curso(
+	id_alunocurso int auto_increment primary key,
+    fk_idaluno int,
+    fk_idcurso int,
+    unique(fk_idaluno,fk_idcurso)
+);
+
+# criando os relacionamentos a traves das chaves estrangeiras 
+alter table aluno_curso add constraint fk_idaluno_alunocurso
+foreign key (fk_idaluno) references aluno(idaluno);
+
+# criando a outra referencia com a tabela curso 
+alter table aluno_curso add constraint fk_idcurso_alunocurso
+foreign key (fk_idcurso) references curso(idcurso)
+
