b3f3ba7164549b16f986b3c04ab7c4fdfea25965
victor moye <51638222+VictorMoye@users.noreply.github.com>
2023-06-04 15:44:05 -0300

    tirando a pasta bkup pois não é necessaria nesse momento ficar no mesmo repositorio do projeto por segurança, e mantendo a entegridade dos relacionamentos 1-1 da tabela endereco aluno fortalecendo transformando a chave estrangeira em unique para mantera a entegridade e evitar erros futuros na tabela

--- modelagem avançando na primeira forma/trabalhando com unique.sql
+++ modelagem avançando na primeira forma/trabalhando com unique.sql
@@ -1,6 +1,10 @@
-select * from aluno 
-desc aluno
+select * from aluno ;
+desc aluno;
 
 # tranforlando o campo cpf e  email da tabela em valor unique 
-alter table aluno add constraint uc_aluno_cpf unique (cpf)
-alter table aluno add constraint uc_aluno_email unique (email)	
+alter table aluno add constraint uc_aluno_cpf unique (cpf);
+alter table aluno add constraint uc_aluno_email unique (email)	;
+
+# para garantir a entegridade do banco de dados e dos relacionamentos 1-1 
+# preciso transformas as chaves estrangeiras em unique tbm. 
+alter table endereco add constraint uc_endereco_fkidaluno unique(fk_idaluno);
