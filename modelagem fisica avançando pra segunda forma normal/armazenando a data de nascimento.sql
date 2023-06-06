19506636c9eb02330bcecbd48a074af77042eb07
victor moye <51638222+VictorMoye@users.noreply.github.com>
2023-06-06 16:59:11 -0300

    acrescentando a data de nascimento dos alunos e inserindo os dados

---
+++ modelagem fisica avançando pra segunda forma normal/armazenando a data de nascimento.sql
@@ -0,0 +1,14 @@
+select * 
+from aluno ;
+
+alter table aluno add data_nascimento date;
+
+select * from aluno;
+
+update aluno set data_nascimento = '1960-05-01' where idaluno = 1;
+update aluno set data_nascimento = '1980-06-12' where idaluno = 2;
+update aluno set data_nascimento = '1982-01-07' where idaluno = 3;
+update aluno set data_nascimento = '1970-03-04' where idaluno = 4;
+update aluno set data_nascimento = '1985-12-05' where idaluno = 5;
+
+select * from aluno;
