b84bb39270b4e6d58aaa032284f55bf19b68a9fa
victor moye <51638222+VictorMoye@users.noreply.github.com>
2023-06-06 16:28:44 -0300

    atualizando os indicativos de pesquisa

--- modelagem fisica avançando pra segunda forma normal/desenvolvendo indicativos de controle de gastos.sql
+++ modelagem fisica avançando pra segunda forma normal/desenvolvendo indicativos de controle de gastos.sql
@@ -49,3 +49,21 @@ select
 	where
         idgasto = (select idgasto from gasto where ano = 2019 and tipo = 'realizado')) as resultado_mar;
         
+select 
+	fev as realizado_fevereiro
+from 
+	gasto 
+where 
+	tipo = ( select idgasto from gasto where idgasto = 2 );
+
+select ago 
+from gasto 
+where tipo = 'realizado';
+
+select ago as agosto_realizado
+from gasto 
+where idgasto = (select idgasto  from gasto where tipo = 'realizado');
+
+select ago as agosto_previsto 
+from gasto 
+where tipo = (select tipo from gasto where idgasto =2);
